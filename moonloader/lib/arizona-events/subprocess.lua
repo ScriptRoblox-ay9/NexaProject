f706daa6898feb62090c1be24812792f0c57b80c
ScriptRoblox-ay9 <tvinkdkl@gmail.com>
2026-06-07 11:57:13 +0300

    Update subprocess.lua

--- moonloader/lib/arizona-events/subprocess.lua
+++ moonloader/lib/arizona-events/subprocess.lua
@@ -1,83 +1,53 @@
 local BitStream = require("arizona-events.bitstream")
 
-local task_t = (function()
-  local ffi = require("ffi")
-
-  local this = {}
-  local meta = {}
-  meta.__index = this
-
-  local queue = {}
-  queue[1] = 1
-
-  local os_clock
-  local table_unpack = table.unpack
-  local select = select
-
-  if jit.os == "Windows" then
-    ffi.cdef "unsigned __stdcall GetTickCount(void);"
-    os_clock = ffi.C.GetTickCount
-  else
-    os_clock = os.clock
-  end
-
-  function this:push(...)
-    self[1] = self[1] + 1
-    self[(self[1])] = {...}
-    return self
-  end
-
-  function this:join()
-    self[2] = os_clock()
-    queue[1] = queue[1] + 1
-    queue[(queue[1])] = self
-  end
-
-  function this:consume()
-    local clock = os_clock()
-    for i = queue[1], 2, -1 do
-      local s = queue[i]
-      local d = clock - s[2]
-      for j = s[1], 3, -1 do
-        local n = s[j]
-        if n[1] < d then
-          if n[6] then
-            n[2](select(3, table_unpack(n)))
-          elseif n[3] then
-            n[2](n[3], n[4], n[5])
-          else
-            n[2]()
-          end
-          s[j] = s[(s[1])]
-          s[1] = s[1] - 1
-        end
-      end
-      if s[1] == 2 then
-        queue[i] = queue[(queue[1])]
-        queue[(queue[1])] = nil
-        queue[1] = queue[1] - 1
-      end
-    end
-  end
-
-  local function constructor()
-    return setmetatable({ 2, 0 }, meta)
-  end
-
-  return setmetatable(this, { __call = constructor })
-end)()
-
 local subprocess = {}
 local thread
 
+
+local pending = {}
+local pending_count = 0
+
+local function flush_pending()
+  if pending_count == 0 then return end
+  local ffi = require("ffi")
+  local clock
+  if jit.os == "Windows" then
+    ffi.cdef "unsigned __stdcall GetTickCount(void);"
+    clock = ffi.C.GetTickCount
+  else
+    clock = function() return math.floor(os.clock() * 1000) end
+  end
+  local now = clock()
+  local i = 1
+  while i <= pending_count do
+    local task = pending[i]
+    
+    if task then
+      local bs = BitStream.new()
+      task.write(bs, task.packet)
+      BitStream.emul(task.id, bs)
+      BitStream.delete(bs)
+      pending[i] = nil
+    end
+    i = i + 1
+  end
+  
+  local new_pending = {}
+  local new_count = 0
+  for j = 1, pending_count do
+    if pending[j] ~= nil then
+      new_count = new_count + 1
+      new_pending[new_count] = pending[j]
+    end
+  end
+  pending = new_pending
+  pending_count = new_count
+end
+
 function subprocess.push(id, packet, write)
-  task_t():push(-1, function()
-    local bs = BitStream.new()
-    write(bs, packet)
-    BitStream.emul(id, bs)
-    BitStream.delete(bs)
-  end):join()
-  if thread:status() ~= "yielded" then
+  pending_count = pending_count + 1
+  pending[pending_count] = { id = id, packet = packet, write = write }
+  if thread and thread:status() ~= "yielded" then
     thread:run()
   end
 end
@@ -85,8 +55,13 @@ end
 if lua_thread then
   thread = lua_thread.create_suspended(function()
     while true do
-      task_t.consume()
-      wait(0)
+      if pending_count > 0 then
+        flush_pending()
+        wait(0)
+      else
+        
+        wait(50)
+      end
     end
   end)
 else
